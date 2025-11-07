a076763c5f0fe4491461084e4ff7de42a36fc5cd
Hlib Hraif <hlib.hraif@protonmail.com>
2025-11-07 10:49:23 +0100

    Switch to Nix

---
+++ flake.nix
@@ -0,0 +1,183 @@
+{
+  description = "zengraf's nix-darwin system flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+    nix-darwin.url = "github:nix-darwin/nix-darwin";
+    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+
+    nix-homebrew.url = "github:zhaofengli/nix-homebrew";
+
+    homebrew-core = {
+      url = "github:homebrew/homebrew-core";
+      flake = false;
+    };
+    homebrew-cask = {
+      url = "github:homebrew/homebrew-cask";
+      flake = false;
+    };
+  };
+
+  outputs = { self, nixpkgs, nix-darwin, home-manager, nix-homebrew
+    , homebrew-core, homebrew-cask }:
+    let
+      system-config = { pkgs, ... }: {
+        nix.enable = false;
+        nix.settings.experimental-features = "nix-command flakes";
+
+        nixpkgs.hostPlatform = "aarch64-darwin";
+        nixpkgs.config.allowUnfree = true;
+
+        security.pam.services.sudo_local = {
+          enable = true;
+          touchIdAuth = true;
+        };
+
+        users.users.zengraf = {
+          shell = pkgs.zsh;
+          home = "/Users/zengraf";
+        };
+
+        environment.systemPackages = [
+          pkgs._1password-cli
+          pkgs.bun
+          pkgs.mas
+          pkgs.ngrok
+          pkgs.nodejs_22
+          pkgs.postgresql
+          pkgs.uv
+          pkgs.ruby
+          pkgs.vim
+          pkgs.zsh
+        ];
+
+        homebrew = {
+          enable = true;
+          brews = [ "sentry-cli" ];
+          casks = [
+            {
+              name = "1password";
+              greedy = true;
+            }
+            "adobe-acrobat-reader"
+            {
+              name = "arc";
+              greedy = true;
+            }
+            {
+              name = "cursor";
+              greedy = true;
+            }
+            "dbeaver-community"
+            "docker-desktop"
+            "drata-agent"
+            "ghostty"
+            "google-drive"
+            "iina"
+            "insomnia"
+            "loom"
+            "moonlight"
+            {
+              name = "notion";
+              greedy = true;
+            }
+            {
+              name = "obsidian";
+              greedy = true;
+            }
+            {
+              name = "slack";
+              greedy = true;
+            }
+            "utm"
+            {
+              name = "zed";
+              greedy = true;
+            }
+            "zoom"
+          ];
+          masApps = {
+            "1Password for Safari" = 1569813296;
+            "DaisyDisk" = 411643860;
+            "Microsoft Word" = 462054704;
+            "Pixelmator Pro" = 1289583905;
+            "Telegram" = 747648890;
+            "Things 3" = 904280696;
+          };
+          onActivation.cleanup = "zap";
+          onActivation.autoUpdate = true;
+          onActivation.upgrade = true;
+        };
+
+        system.defaults = {
+          dock.autohide = true;
+          dock.persistent-apps = [
+            "/Applications/Arc.app"
+            "/Applications/Telegram.app"
+            "/Applications/Slack.app"
+            "/Applications/Things3.app"
+            "/Applications/1Password.app"
+            "/Applications/Obsidian.app"
+            "/Applications/Notion.app"
+            "/Applications/Zed.app"
+            "/Applications/Cursor.app"
+            "/Applications/Ghostty.app"
+          ];
+          dock.show-recents = false;
+          finder.FXPreferredViewStyle = "clmv";
+          loginwindow.GuestEnabled = false;
+          NSGlobalDomain.AppleInterfaceStyle = "Dark";
+          NSGlobalDomain.InitialKeyRepeat = 25;
+          NSGlobalDomain.KeyRepeat = 2;
+        };
+
+        system.activationScripts.extraActivation = {
+          text = ''
+            if [[ $(uname -m) == "arm64" ]] && ! pgrep oahd >/dev/null 2>&1; then
+              echo "installing Rosetta 2..."
+              /usr/sbin/softwareupdate --install-rosetta --agree-to-license
+            fi
+          '';
+        };
+
+        system.primaryUser = "zengraf";
+        system.configurationRevision = self.rev or self.dirtyRev or null;
+        system.stateVersion = 6;
+      };
+
+      home-config = {
+        home-manager.useGlobalPkgs = true;
+        home-manager.useUserPackages = true;
+        home-manager.users.zengraf = ./home.nix;
+
+        home-manager.backupFileExtension = "bak";
+      };
+
+      homebrew-config = {
+        nix-homebrew = {
+          enable = true;
+          enableRosetta = true;
+          user = "zengraf";
+          taps = {
+            "homebrew/homebrew-core" = homebrew-core;
+            "homebrew/homebrew-cask" = homebrew-cask;
+          };
+          mutableTaps = false;
+        };
+      };
+    in {
+      darwinConfigurations.macbook-m4 = nix-darwin.lib.darwinSystem {
+        modules = [
+          system-config
+          home-config
+          homebrew-config
+          home-manager.darwinModules.home-manager
+          nix-homebrew.darwinModules.nix-homebrew
+        ];
+      };
+    };
+}
---
+++ home.nix
@@ -0,0 +1,150 @@
+{ config, pkgs, ... }:
+
+let
+  mkBiomeConfig = lang: {
+    name = lang;
+    value = { formatter = { language_server = { name = "biome"; }; }; };
+  };
+
+  mkBiomeConfigWithActions = lang: {
+    name = lang;
+    value = {
+      formatter = { language_server = { name = "biome"; }; };
+      code_actions_on_format = {
+        "source.fixAll.biome" = true;
+        "source.organizeImports.biome" = true;
+      };
+    };
+  };
+
+  biomeLanguagesWithActions = [ "JavaScript" "TypeScript" "TSX" ];
+
+  biomeLanguagesOnly = [ "JSON" "JSONC" "CSS" "GraphQL" ];
+
+  biomeConfigWithActions = builtins.listToAttrs
+    (map mkBiomeConfigWithActions biomeLanguagesWithActions);
+  biomeConfigOnly = builtins.listToAttrs (map mkBiomeConfig biomeLanguagesOnly);
+
+  biomeConfig = biomeConfigWithActions // biomeConfigOnly;
+in {
+  home.stateVersion = "25.05";
+
+  home.username = "zengraf";
+  home.homeDirectory = "/Users/zengraf";
+
+  home.packages = [
+    pkgs.claude-code
+    pkgs.delta
+    pkgs.direnv
+    pkgs.fzf
+    pkgs.google-cloud-sdk
+    pkgs.gnupg
+    pkgs.nil
+    pkgs.nixfmt-classic
+    pkgs.tig
+  ];
+
+  programs.git = {
+    enable = true;
+
+    extraConfig = {
+      user.name = "Hlib Hraif";
+      user.email = "hlib.hraif@protonmail.com";
+      user.signingkey = "CCAA175508934029";
+
+      commit.gpgsign = true;
+
+      core.pager = "delta";
+      interactive.diffFilter = "delta --color-only";
+      delta.navigate = true;
+      merge.conflictStyle = "zdiff3";
+    };
+  };
+
+  programs.zsh = {
+    enable = true;
+
+    oh-my-zsh = {
+      enable = true;
+      theme = "robbyrussell";
+      plugins = [ "git" "direnv" "docker-compose" "macos" "sudo" "z" ];
+    };
+
+    initContent = ''
+      export EDITOR="vim"
+      source <(fzf --zsh)
+      source $HOME/.config/op/plugins.sh
+    '';
+  };
+
+  programs.zed-editor = {
+    enable = true;
+
+    extensions =
+      [ "biome" "dockerfile" "html" "make" "nix" "prisma" "scss" "sql" "toml" ];
+
+    userSettings = {
+      features = { edit_prediction_provider = "copilot"; };
+
+      agent = {
+        default_profile = "write";
+        default_model = {
+          provider = "copilot_chat";
+          model = "gpt-4o";
+        };
+      };
+
+      autosave = "on_focus_change";
+      vim_mode = true;
+      ui_font_size = 16;
+      buffer_font_size = 16;
+      theme = {
+        mode = "system";
+        light = "One Light";
+        dark = "One Dark";
+      };
+
+      languages = biomeConfig // {
+        Nix = {
+          tab_size = 2;
+          language_servers = [ "nil" "!nixd" ];
+        };
+        Python = {
+          language_servers = [ "pyright" "ruff" ];
+          formatter = [
+            {
+              code_actions = {
+                "source.organizeImports.ruff" = true;
+                "source.fixAll.ruff" = true;
+              };
+            }
+            { language_server.name = "ruff"; }
+            {
+              external = {
+                command = "black";
+                arguments = [ "-" ];
+              };
+            }
+          ];
+        };
+        Ruby = { language_servers = [ "ruby-lsp" "rubocop" "!solargraph" ]; };
+      };
+
+      lsp = {
+        biome = { settings = { require_config_file = true; }; };
+        nil = {
+          initialization_options = { formatting.command = [ "nixfmt" ]; };
+        };
+        pylsp = { initialization_options = { plugins.mypy.enabled = true; }; };
+        rubocop = { initialization_options = { safeAutocorrect = false; }; };
+        ruby-lsp = {
+          initialization_options = {
+            enabledFeatures = { diagnostics = false; };
+          };
+        };
+      };
+    };
+  };
+
+  programs.home-manager.enable = true;
+}
