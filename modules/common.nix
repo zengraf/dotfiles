32ade5bf5804b8266f7ab3eede5f7fbe7cbb3a9b
Hlib Hraif <hlib.hraif@protonmail.com>
2026-04-10 14:37:05 +0200

    Fix the nix package setting

--- modules/common.nix
+++ modules/common.nix
@@ -1,6 +1,6 @@
 { pkgs, hostname, ... }: {
+  nix.package = pkgs.lix;
   nix.settings = {
-    package = pkgs.lix;
     experimental-features = "nix-command flakes";
     substituters = [ "https://zengraf.cachix.org" ];
     trusted-public-keys = [ "zengraf.cachix.org-1:NhxoewsqkPaN9Tp8Q0A5XR0+bgwj4JM8atN8cfq87PI=" ];
