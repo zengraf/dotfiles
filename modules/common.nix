3dcf1552fe1f9add165921ba692156dc7a400277
Hlib Hraif <hlib.hraif@protonmail.com>
2026-04-30 17:35:14 +0200

    Add a trusted user to nix config.

--- modules/common.nix
+++ modules/common.nix
@@ -1,9 +1,17 @@
-{ pkgs, hostname, ... }: {
+{
+  pkgs,
+  lib,
+  hostname,
+  username ? null,
+  ...
+}:
+{
   nix.package = pkgs.lixPackageSets.stable.lix;
   nix.settings = {
     experimental-features = "nix-command flakes";
     substituters = [ "https://zengraf.cachix.org" ];
     trusted-public-keys = [ "zengraf.cachix.org-1:NhxoewsqkPaN9Tp8Q0A5XR0+bgwj4JM8atN8cfq87PI=" ];
+    trusted-users = lib.optionals (username != null) [ username ];
   };
 
   nixpkgs.config.allowUnfree = true;
@@ -13,5 +21,10 @@
 
   security.pki.certificateFiles = [ ../pki/ca.crt ];
 
-  environment.systemPackages = with pkgs; [ cachix nixos-rebuild vim wget ];
+  environment.systemPackages = with pkgs; [
+    cachix
+    nixos-rebuild
+    vim
+    wget
+  ];
 }
