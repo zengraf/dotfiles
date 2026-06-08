bfaa604e9841748f618b1f867ad245fe0dafdaff
Hlib Hraif <hlib.hraif@protonmail.com>
2026-06-08 13:54:42 +0200

    Remove private cache, speed up evaluation

--- modules/common.nix
+++ modules/common.nix
@@ -9,8 +9,17 @@
   nix.package = pkgs.lixPackageSets.stable.lix;
   nix.settings = {
     experimental-features = "nix-command flakes";
-    substituters = [ "https://zengraf.cachix.org" ];
-    trusted-public-keys = [ "zengraf.cachix.org-1:NhxoewsqkPaN9Tp8Q0A5XR0+bgwj4JM8atN8cfq87PI=" ];
+    substituters = [
+      "https://cache.nixos.org"
+      "https://nix-community.cachix.org"
+    ];
+    trusted-public-keys = [
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
+    connect-timeout = 5;
+    stalled-download-timeout = 20;
+    download-attempts = 2;
     trusted-users = lib.optionals (username != null) [ username ];
   };
 
